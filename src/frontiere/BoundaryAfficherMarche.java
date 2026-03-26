98fb571c691d683cc9c4309bf1c1ad8b7265603f
molletevan3106 <mollet.evan3106@gmail.com>
2026-03-26 11:17:44 +0100

    Cas 7 sur Afficher Marché fini

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -1,5 +1,7 @@
 package frontiere;
 
+import java.util.Iterator;
+
 import controleur.ControlAfficherMarche;
 
 public class BoundaryAfficherMarche {
@@ -10,6 +12,21 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche;
+		String vendeur;
+		String quantite;
+		String produit;
+		infosMarche=controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length==0) {
+			System.out.println("Le marché est vide, revenez plus tard.\n");
+		}else {
+			System.out.println(nomAcheteur+" vous trouverez au marché :");
+			for (int i=0; i+2 < infosMarche.length&&infosMarche[i+2]!=null;i+=3) {
+				vendeur=infosMarche[i];
+				quantite=infosMarche[i+1];
+				produit=infosMarche[i+2];
+				System.out.println("- "+vendeur+" qui vend "+quantite+" "+produit+".");
+			}
+		}
 	}
 }
