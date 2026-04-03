efd238da5ee797c2d4dbd3a18c59630210eb55c8
molletevan3106 <mollet.evan3106@gmail.com>
2026-04-03 14:40:08 +0200

    Création de test pour Controleur Emmenager et VerificationIdentité

--- test/controleur/ControlEmmenagerTest.java
+++ test/controleur/ControlEmmenagerTest.java
@@ -2,28 +2,54 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControlEmmenagerTest {
+import personnages.Chef;
+import villagegaulois.Village;
 
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation ...");
+		village= new Village("le village des irréductibles",10,5);
+		abraracourcix=new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+	
 	@Test
 	void testControlEmmenager() {
-		fail("Not yet implemented");
+		ControlEmmenager controlEmmenager= new ControlEmmenager(village);
+		assertNotNull(controlEmmenager, "Constructeur ne renvoie pas null");
 	}
 
 	@Test
 	void testIsHabitant() {
-		fail("Not yet implemented");
+		ControlEmmenager controlEmmenager= new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+		assertFalse(controlEmmenager.isHabitant("Existe pas"));
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
 	}
 
 	@Test
 	void testAjouterDruide() {
-		fail("Not yet implemented");
+		ControlEmmenager controlEmmenager= new ControlEmmenager(village);
+		assertFalse(controlEmmenager.isHabitant("Panoramix"));
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
 	}
 
 	@Test
 	void testAjouterGaulois() {
-		fail("Not yet implemented");
+		ControlEmmenager controlEmmenager= new ControlEmmenager(village);
+		assertFalse(controlEmmenager.isHabitant("Bonemine"));
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
 	}
 
 }
